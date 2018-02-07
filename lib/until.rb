159ccbcb3c80703f8426f335d8c7b1f2816f7519
Ed Wright <ew_wrightiii@live.com>
2018-02-07 07:41:35 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,9 @@
 def using_until
         levitation_force = 6
         #run while condition is false
-        
+        puts "Wingardium Leviosa"
         until levitation_force = 10 do 
-          puts "Wingardium Leviosa"
+          
           levitation_force += 1
         end
         
